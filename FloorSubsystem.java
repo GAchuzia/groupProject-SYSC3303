f05b0f05c6496269f9422e457b0d6c72265e38d4
sajafawagreh <saja.fawagreh@gmail.com>
2024-01-29 16:05:04 -0500

    Added my name and Javeria's on each class

--- FloorSubsystem.java
+++ FloorSubsystem.java
@@ -1,6 +1,8 @@
 /**
  * This class represents the floor subsystem, which will read the elevator usage data.
  * @author Matteo Golin, 101220709
+ * @author Saja Fawagreh, 101217326
+ * @author Javeria Sohail, 101197163
  * @version 0.0.0
  */
 public class FloorSubsystem implements Runnable {
