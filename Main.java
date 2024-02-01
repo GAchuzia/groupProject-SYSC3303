24738f87308ccee536e950869987d10c472b2ff2
Matteo Golin <matteo.golin@gmail.com>
2024-02-01 13:36:28 -0500

    Add the remainder of the group member names.

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ import java.io.FileNotFoundException;
  * The main file of the program.
  *
  * @author Matteo Golin, 101220709
+ * @author Grant Achuzia, 101222695
+ * @author Saja Fawagreh, 101217326
+ * @author Javeria Sohail, 101197163
+ * @author Yousef Hammad, 101217858
  * @version 0.0.0
  */
 public class Main {
