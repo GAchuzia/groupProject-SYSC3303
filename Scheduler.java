04e22687770ae7f475655e627b8cf933ed228896
Yousef Hammad <yousef.h24@hotmail.com>
2024-02-02 10:23:23 -0500

    Update Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -1,3 +1,5 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 /**
  * The scheduler that will be responsible for assigning the correct elevators to the correct floor.
  * @author Matteo Golin, 101220709
@@ -7,8 +9,28 @@
  * @author Yousef Hammad, 101217858
  * @version 0.0.0
  */
+
 public class Scheduler implements Runnable {
+
+    private MessageQueue messageQ;
+
+    public Scheduler (MessageQueue messageQ){
+        this.messageQ = messageQ;
+    }
+    @Override
     public void run() {
-        return;
+        //NOTE: Need to create floorMessagesSize and elevatorMessagesSize methods in MessageQueue
+        while (messageQ.floorMessagesSize() > 0 || messageQ.elevatorMessagesSize() > 0){
+            //NOTE: need to create checkFloor methods in MessageQueue
+            if (messageQ.checkFloor()){
+                String floorMessage = messageQ.getFromFloor();
+                messageQ.putFromElevator(floorMessage);
+            }
+            //NOTE: need to create checkEelevator method in messageQueue
+            if (messageQ.checkEelevator()){
+                String elevatorMessage = messageQ.getFromElevator();
+                messageQ.putFromFloor(elevatorMessage);
+            }
+        }
     }
 }
