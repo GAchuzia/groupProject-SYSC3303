ea67d357e14c2b42e0bf37d5670e320b9b714a20
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2024-02-03 00:39:50 -0500

    Add INCOMPLETE test cases for the FloorSubsytem and Scheduler classes

--- FloorSubsystemTest.java
+++ FloorSubsystemTest.java
@@ -1,4 +1,40 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FloorSubsystemTest {
-  
+
+    @Test
+    void run() throws InterruptedException{
+        // Initialize objects needed for the FloorSubsytem
+        MessageQueue<ElevatorRequest> floorIncoming = new MessageQueue<>();
+        MessageQueue<ElevatorRequest> floorOutgoing = new MessageQueue<>();
+        FloorSubsystem test_fsys;
+        try {
+            test_fsys = new FloorSubsystem("./testdata.txt", floorIncoming, floorOutgoing);
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open input file.");
+            return;
+        }
+
+        // Start the floor subsystem to process the message
+        Thread floorThread = new Thread(test_fsys);
+        floorThread.start();
+        floorIncoming.putMessage(new ElevatorRequest("14:05:15.0 2 Up 4"));
+        Thread.sleep(300);
+
+        // Check that FloorSubsystem put the request in the outgoing queue
+        assertFalse(floorOutgoing.isEmpty());
+
+        assertEquals(new ElevatorRequest("14:05:15.0 2 Up 4"), floorOutgoing.getMessage());
+
+        // Check that the outgoing queue is empty once the FloorSubsystem exits
+        assertNull(floorOutgoing.getMessage());
+
+        // Stop the FloorSubsystem
+        floorIncoming.putMessage(null);
+        floorThread.join();
+    }
 }
--- SchedulerTest.java
+++ SchedulerTest.java
@@ -1,4 +1,43 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SchedulerTest {
-  
+
+    @Test
+    void run() throws InterruptedException{
+        //Initialize MessageQueues for the test
+        MessageQueue<ElevatorRequest> floorIncoming = new MessageQueue<>();
+        MessageQueue<ElevatorRequest> floorOutgoing = new MessageQueue<>();
+        MessageQueue<ElevatorRequest> elevatorIncoming = new MessageQueue<>();
+        MessageQueue<ElevatorRequest> elevatorOutgoing = new MessageQueue<>();
+
+        // Make and start a scheduler thread
+        Scheduler scheduler = new Scheduler(floorIncoming, floorOutgoing, elevatorIncoming, elevatorOutgoing);
+        Thread schedulerThread = new Thread(scheduler);
+        schedulerThread.start();
+
+        // Send a message to the elevator from the scheduler
+        ElevatorRequest floorMessage = new ElevatorRequest("14:05:15.0 2 Up 4");
+        floorIncoming.putMessage(floorMessage);
+        Thread.sleep(300);
+
+        // Check that scheduler sent message to the elevator
+        assertFalse(elevatorOutgoing.isEmpty());
+        assertEquals(floorMessage, elevatorOutgoing.getMessage());
+
+        // Send a message from the elevator to the scheduler
+        ElevatorRequest elevatorMessage = new ElevatorRequest("14:10:00.0 4 Down 1");
+        elevatorIncoming.putMessage(elevatorMessage);
+        Thread.sleep(300);
+
+        // Check that the scheduler forwarded the message to the floor
+        assertFalse(floorOutgoing.isEmpty());
+        assertEquals(elevatorMessage, floorOutgoing.getMessage());
+
+        // Stop the scheduler
+        elevatorIncoming.putMessage(null);
+        Thread.sleep(300);
+        schedulerThread.join();
+    }
 }
