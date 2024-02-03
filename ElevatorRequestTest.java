099b15e675e2fe65c560550e2460ee5b09265b5b
Matteo Golin <matteo.golin@gmail.com>
2024-02-03 16:30:08 -0500

    Filled out test suite to test all edge cases and functionality.

--- ElevatorRequestTest.java
+++ ElevatorRequestTest.java
@@ -1,15 +1,54 @@
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Contains the test suite for the ElevatorRequest class.
+ *
+ * @author Matteo Golin, 101220709
+ * @author Grant Achuzia, 101222695
+ * @author Saja Fawagreh, 101217326
+ * @author Javeria Sohail, 101197163
+ * @author Yousef Hammad, 101217858
+ * @version 0.0.0
+ */
 class ElevatorRequestTest {
 
+    /**
+     * A sample elevator request to perform testing on.
+     */
+    static final ElevatorRequest REQUEST = new ElevatorRequest("14:05:15.2 2 Up 4");
+
+    /**
+     * Check that the fields of the elevator request can be correctly filled from a parsed input string.
+     */
+    @Test
+    void testParsing() {
+        assertEquals(REQUEST.getDirection(), Direction.Up);
+        assertEquals(REQUEST.getOriginFloor(), 2);
+        assertEquals(REQUEST.getDestinationFloor(), 4);
+        assertEquals(REQUEST.getTimestamp(), LocalTime.of(14, 5, 15, 200000000));
+    }
+
+    /**
+     * Check that the string representation of the elevator request is generated correctly.
+     */
     @Test
     void testToString() {
-        // Make a test ElevatorRequest and compare it to an expected ElevatorRequest using the toString method
-        String testString = "14:05:15.2 2 Up 4";
-        ElevatorRequest elevatorRequest = new ElevatorRequest(testString);
-        String expectedResult = "Timestamp: 14:05:15.200 Direction: Up To: 4 From: 2";
-        assertEquals(expectedResult, elevatorRequest.toString());
+        assertEquals("Timestamp: 14:05:15.200 Direction: Up To: 4 From: 2", REQUEST.toString());
+    }
+
+    /**
+     * Check that passing a bad input string to the ElevatorRequest constructor results in an error.
+     */
+    @Test
+    void testBadConstruction() {
+        assertThrows(DateTimeParseException.class, () -> {
+            new ElevatorRequest("this is a bad string");
+        });
     }
 }
