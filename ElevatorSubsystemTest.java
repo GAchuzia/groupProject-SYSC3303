b66c9debc3ac376d4c3201dd782a3db31b61a843
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2024-02-02 23:21:29 -0500

    Add test case for the run method for the ElevatorSubsystem class

--- ElevatorSubsystemTest.java
+++ ElevatorSubsystemTest.java
@@ -1,4 +1,30 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ElevatorSubsystemTest {
-  
+
+    @Test
+    void run() throws InterruptedException {
+        // Initialize objects needed for the elevator sim
+        MessageQueue<ElevatorRequest> incoming = new MessageQueue<>();
+        MessageQueue<ElevatorRequest> outgoing = new MessageQueue<>();
+        ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem(incoming, outgoing);
+        ElevatorRequest elevatorRequest = new ElevatorRequest("23:10:12.1 3 Down 2");
+        Thread elevatorThread = new Thread(elevatorSubsystem);
+
+        // Start the elevator susystem to process the message
+        elevatorThread.start();
+        incoming.putMessage(elevatorRequest);
+        Thread.sleep(300); // Gives the ElevatorSubsytem time to process the message
+
+        // Check that the ElevatorSubsystem put the request in the outgoing queue
+        assertFalse(outgoing.isEmpty());
+        assertEquals(elevatorRequest, outgoing.getMessage());
+
+        // Check that the outgoing queue is empty once the ElevatorSubsystem exits
+        assertTrue(outgoing.isEmpty());
+
+
+    }
 }
