687dbb953a2169ec32f39b005ddc8c9f645e6442
Matteo Golin <matteo.golin@gmail.com>
2024-02-03 16:29:19 -0500

    Put direction enum in its own file.

---
+++ Direction.java
@@ -0,0 +1,14 @@
+/**
+ * Represents a direction for the elevator to travel in.
+ *
+ * @author Matteo Golin, 101220709
+ * @author Grant Achuzia, 101222695
+ * @author Saja Fawagreh, 101217326
+ * @author Javeria Sohail, 101197163
+ * @author Yousef Hammad, 101217858
+ * @version 0.0.0
+ */
+public enum Direction {
+    Up,
+    Down,
+}
