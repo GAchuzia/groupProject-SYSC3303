2ff8c9eaabeb4a64d44e323eb0bc6ce1fdcc7e2e
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2024-04-10 00:35:07 -0400

    feat: Add new test for the Scheduler class

--- SchedulerTest.java
+++ SchedulerTest.java
@@ -1,7 +1,5 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -16,81 +14,40 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Yousef Hammad, 101217858
  * @version 0.0.0
  */
-//
-//class SchedulerTest {
-//
-//    /**
-//     * The message queue for messages going into the FloorSubsystem.
-//     */
-//    private MessageQueue<ElevatorRequest> floorIncoming;
-//    /**
-//     * The message queue for messages coming out of the FloorSubsystem.
-//     */
-//    private MessageQueue<ElevatorRequest> floorOutgoing;
-//    /**
-//     * The message queue for messages going into the ElevatorSubsystem.
-//     */
-//    private MessageQueue<ElevatorRequest> elevatorIncoming;
-//    /**
-//     * The message queue for messages going out of the ElevatorSubsystem.
-//     */
-//    private MessageQueue<ElevatorRequest> elevatorOutgoing;
-//
-//    /**
-//     * The scheduler subsystem itself.
-//     */
-//    private Scheduler scheduler;
-//
-//    /**
-//     * Initialize scheduler with the message queues.
-//     */
-//    @BeforeEach
-//    void setUp() {
-//        this.floorIncoming = new MessageQueue<>();
-//        this.floorOutgoing = new MessageQueue<>();
-//        this.elevatorIncoming = new MessageQueue<>();
-//        this.elevatorOutgoing = new MessageQueue<>();
-//    }
-//
-//    /**
-//     * Wipe all message queues and subsystems to be newly constructed before the next test.
-//     */
-//    @AfterEach
-//    void tearDown() {
-//        this.floorIncoming = null;
-//        this.floorOutgoing = null;
-//        this.elevatorIncoming = null;
-//        this.elevatorOutgoing = null;
-//        this.scheduler = null;
-//    }
-//
+
+class SchedulerTest {
+    /**
+     * Check that elevator status records are updated based on elevator requests.
+     */
+    @Test
+    public void testUpdateRecords() {
+        ElevatorStatus[] statuses = new ElevatorStatus[3];
+        for (int i = 0; i < statuses.length; i++) {
+            statuses[i] = new ElevatorStatus();
+        }
+
+        ElevatorRequest response = new ElevatorRequest(1, 1, 5, 2, 0, 5, 1, false);
+        Scheduler.updateRecords(statuses, response);
+
+        assertEquals(1, statuses[1].getElevator());
+        assertEquals(1, statuses[1].getFloor());
+        assertEquals(Direction.Up, statuses[1].getDirection());
+        assertEquals(2, statuses[1].getRiders());
+        assertEquals(0, statuses[1].getDoor());
+        assertEquals(5, statuses[1].getDestinationFloor());
+        assertEquals(1, statuses[1].getOriginFloor());
+        assertEquals(false, statuses[1].isComplete());
+    }
+
+
+
+    /**
+     * Tests that selectElevator selects the closest elevator regardless of direction
+     * for a request if there are no elevators going in the desired direction.
+     */
 //    @Test
-//    void run() throws InterruptedException {
-//        // Make and start a scheduler thread
-//        Thread schedulerThread = new Thread(String.valueOf(scheduler));
-//        schedulerThread.start();
-//
-//        // Send a message to the elevator from the floor
-//        ElevatorRequest floorMessage = new ElevatorRequest("14:05:15.0 2 Up 4");
-//        floorOutgoing.putMessage(floorMessage);
-//        Thread.sleep(300);
-//
-//        // Check that scheduler sent message to the elevator
-//        assertFalse(elevatorIncoming.isEmpty());
-//        assertEquals(floorMessage, elevatorIncoming.getMessage());
-//
-//        // Send a message from the elevator to the scheduler
-//        ElevatorRequest elevatorMessage = new ElevatorRequest("14:10:00.0 4 Down 1");
-//        elevatorOutgoing.putMessage(elevatorMessage);
-//        Thread.sleep(300);
-//
-//        // Check that the scheduler forwarded the message to the floor
-//        assertFalse(floorIncoming.isEmpty());
-//        assertEquals(elevatorMessage, floorIncoming.getMessage());
-//
-//        // Stop the scheduler
-//        elevatorOutgoing.putMessage(null);
-//        Thread.sleep(300);
-//        schedulerThread.join();
+//    public void testSelectElevator() {
 //    }
-//}
+
+
+}
