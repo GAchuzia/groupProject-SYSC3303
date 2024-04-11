203a9b1a526e0d6139bb76a0bbff94494e60c323
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2024-04-10 21:38:08 -0400

    fix: Update RequestProgressWrapper test class with updated parameters for ElevatorRequest

--- RequestProgressWrapperTest.java
+++ RequestProgressWrapperTest.java
@@ -1,50 +1,46 @@
-//import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.jupiter.api.Test;
-//
-//class RequestProgressWrapperTest {
-//    /**
-//     *  Test that the wrapper tracks the elevator's progress correctly
-//     */
-//    @Test
-//    void testUpdateProgress() {
-//        // Create a request from floor 1 to floor 5 with 3 riders
-//        ElevatorRequest request = new ElevatorRequest(1, 1, 7, 4);
-//        RequestProgressWrapper wrapper = new RequestProgressWrapper(request);
-//
-//        // Request should not be picked up yet
-//        assertFalse(wrapper.wasPickedUp());
-//        // Request shouldn't be dropped  off yet
-//        assertFalse(wrapper.wasDroppedOff());
-//        // Request should be incomplete
-//        assertFalse(wrapper.isComplete());
-//
-//        // Update progress to floor 1 (pick-up)
-//        wrapper.updateProgress(1);
-//        // Request should be picked up once original floor is visited
-//        assertTrue(wrapper.wasPickedUp());
-//        // Request shouldn't be dropped  off yet
-//        assertFalse(wrapper.wasDroppedOff());
-//        // Request should still be incomplete
-//        assertFalse(wrapper.isComplete());
-//
-//        // Update progress to floor 5 (drop-off)
-//        wrapper.updateProgress(7);
-//        // Request should be dropped pff after visitation floor has been visited
-//        assertTrue(wrapper.wasDroppedOff());
-//        // Request should now be complete
-//        assertTrue(wrapper.isComplete());
-//    }
-//
-//    /**
-//     *  Test that the wrapper returns the ElevatorRequest
-//     */
-//    @Test
-//    void testGetRequest() {
-//        // Create a request
-//        ElevatorRequest request = new ElevatorRequest(2, 2, 4, 2);
-//        RequestProgressWrapper wrapper = new RequestProgressWrapper(request);
-//
-//        // Check if the request is correct
-//        assertEquals(request, wrapper.getRequest());
-//    }
-//}
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class RequestProgressWrapperTest {
+    /**
+     *  Test that the wrapper tracks the elevator's progress correctly as it
+     *  picks up and drops off a request.
+     */
+    @Test
+    void testUpdateProgress() {
+        // Elevator request for the 4th floor going to the 6th floor
+        ElevatorRequest request = new ElevatorRequest(1, 4, 7, 2, 0, 7, 1, false);
+        RequestProgressWrapper wrapper = new RequestProgressWrapper(request);
+
+        // Request should not be picked up or dropped off yet
+        assertFalse(wrapper.wasPickedUp());
+        assertFalse(wrapper.wasDroppedOff());
+        assertFalse(wrapper.isComplete());
+
+        // Elevator picks up the request from the 4th floor
+        wrapper.updateProgress(4);
+        assertTrue(wrapper.wasPickedUp());
+        assertFalse(wrapper.wasDroppedOff());
+        assertFalse(wrapper.isComplete());
+
+        // Elevator drops off request on the 7th floor
+        wrapper.updateProgress(7);
+        assertTrue(wrapper.wasPickedUp());
+        assertTrue(wrapper.wasDroppedOff());
+        assertTrue(wrapper.isComplete());
+    }
+
+    /**
+     *  Test that the wrapper returns the correct elevator request
+     */
+    @Test
+    void testGetRequest() {
+        // Create an elevator request
+        ElevatorRequest request = new ElevatorRequest(1, 3, 6, 2, 0, 6, 1, false);
+        RequestProgressWrapper wrapper = new RequestProgressWrapper(request);
+
+        // Make sure wrapped request is the same as original request
+        ElevatorRequest wrappedRequest = wrapper.getRequest();
+        assertEquals(request, wrappedRequest);
+    }
+}
