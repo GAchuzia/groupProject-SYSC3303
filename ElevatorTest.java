0a138d09e2fae313fef0a7dcae00200d32119ffb
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2024-04-10 22:57:46 -0400

    fix: Readd the testMove and testUpdateRequests method because I mistakenly deleted them both trying to resolve a merge conflict

--- ElevatorTest.java
+++ ElevatorTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Test suite for testing the functionality of the Elevator class.
@@ -17,7 +18,30 @@ import java.net.*;
 public class ElevatorTest {
     private Elevator elevator;
 
+    /**
+     * Test that elevator correctly moves to the next floor in the right direction.
+     * Also checks that the elevator does not try to move below the ground floor.
+     */
+    @Test
+    public void testMove() throws SocketException {
+        elevator = new Elevator(1998);
 
+        // Elevator moves up a floor
+        elevator.move(6); // randomNumber > 5 to not trigger fault
+        assertEquals(2, elevator.getFloor());
+
+        // Elevator moves down a floor
+        elevator.toggleDirection();
+        elevator.move(6);
+        assertEquals(1, elevator.getFloor());
+
+        // Elevator tries to go below ground floor
+        assertFalse(elevator.move(6));
+
+        // Fault is triggered
+        elevator.toggleDirection();
+        assertFalse(elevator.move(1)); // randomNumber < 5 to trigger fault
+    }
 
     /**
      * Test that a random number greater than 30 results in the doors successfully
@@ -80,7 +104,48 @@ public class ElevatorTest {
     public void testAtStop() throws SocketException {
         elevator = new Elevator(2008);
 
-        elevator.move(elevator.nextRandomNum());
+        elevator.move(90);
         assertFalse(elevator.atStop());
+    }
+
+
+    /**
+     * Tests that the elevator sends a completion message to the ElevatorSubsystem
+     * when it is finished a request.
+     * WARNING: The port `ElevatorSubsystem.PORT` cannot be in use when this test is
+     * run.
+     */
+    @Test
+    public void testUpdateRequests() throws IOException, InterruptedException {
+        int port = 2009;
+        elevator = new Elevator(port);
+
+        // Test request for the elevator to process
+        ElevatorRequest request = new ElevatorRequest("14:05:15.0 2 Up 4");
+
+        // Mock ElevatorSubsystem
+        DatagramSocket socket = new DatagramSocket(ElevatorSubsystem.PORT);
+
+        // Send the request to the elevator
+        new Thread(elevator).start();
+        byte[] data = request.getBytes();
+        DatagramPacket p = new DatagramPacket(data, data.length);
+        p.setPort(port);
+        p.setAddress(InetAddress.getLocalHost());
+        socket.send(p);
+
+
+        // Wait to receive the response from the elevator that the request is complete
+        DatagramPacket response = new DatagramPacket(new byte[100], 100);
+
+        socket.receive(response);
+        ElevatorRequest parsed_response = new ElevatorRequest(response.getData());
+
+        while (!parsed_response.isComplete()) {
+            socket.receive(response);
+            parsed_response = new ElevatorRequest(response.getData());
+        }
+        assertTrue(parsed_response.isComplete());
+        assertEquals(request.getTimestamp(), parsed_response.getTimestamp());
     }
 }
